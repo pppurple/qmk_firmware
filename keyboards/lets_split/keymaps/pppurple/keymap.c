945f3a6becd36219e560466c1f4112742e62acaa
pppurple <shuggie.otis0@gmail.com>
2017-09-04 02:48:33 +0900

    update

--- keyboards/lets_split/keymaps/pppurple/keymap.c
+++ keyboards/lets_split/keymaps/pppurple/keymap.c
@@ -12,6 +12,7 @@ extern keymap_config_t keymap_config;
 #define _QWERTY 0
 #define _LOWER 1
 #define _RAISE 2
+#define _UNDERGLOW 3
 #define _ADJUST 16
 
 enum custom_keycodes {
@@ -19,6 +20,7 @@ enum custom_keycodes {
   LOWER,
   RAISE,
   ADJUST,
+  UNDERGLOW,
 };
 
 // Fillers to make layering more clear
@@ -42,9 +44,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * | Esc  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  | Enter|
+ * | Ctrl/|   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  | Enter|
+ * | Tab  |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  | Up   |
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  | Shift|
+ * |      |      |      |      |      |      |      |      |      |      |      | /Up  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | GUI  |  Fn  | Alt  | MHEN |Lower |Space |Space |Raise | HENK | Left | Down |Right |
  * `-----------------------------------------------------------------------------------'
@@ -54,17 +58,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = KEYMAP( \
   KC_ESC,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
   KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT, \
-  KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_UP, \
+  KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_T(KC_UP), \
   KC_LGUI,  _______, KC_LALT, KC_LANG2,LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LANG1,KC_LEFT ,KC_DOWN, KC_RGHT \
 ),
 */
 
 // Win
 [_QWERTY] = KEYMAP( \
-  KC_ESC,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
-  KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT, \
-  KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_UP, \
-  KC_LGUI,  _______, KC_LALT, KC_MHEN, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_HENK, KC_LEFT ,KC_DOWN, KC_RGHT \
+  KC_ESC,       KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
+  CTL_T(KC_TAB),KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT, \
+  KC_LSFT,      KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_UP, \
+  KC_LGUI,      _______, KC_LALT, KC_MHEN, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_HENK, KC_LEFT ,KC_DOWN, KC_RGHT \
 ),
 
 /*[_QWERTY] = KEYMAP( \
@@ -76,9 +80,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 */
 /* Raise
  * ,-----------------------------------------------------------------------------------.
- * | Tab  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Del  |
+ * |      |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Del  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |      |
+ * | Tab  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |  F11 |  F12 |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -86,8 +90,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = KEYMAP( \
-  KC_TAB,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
-  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______, \
+  _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
+  KC_TAB,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______, \
   _______, KC_F11,  KC_F12,  _______, _______, _______, _______, _______, _______, _______, _______, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
 ),
@@ -105,16 +109,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |ISO # |ISO / |   ^  |   *  |   +  |  {   |   }  |   -  |   @  |   `  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |ISO ~ |ISO | |      |      |      |  [   |   ]  |   ¥  |   \  |  _   |
+ * |      |      |ISO ~ |ISO | |      |      |      |  [   |   ]  |   _  |   \  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = KEYMAP( \
-  KC_EXLM, KC_QUOT, KC_HASH,  KC_DLR,    KC_PERC, KC_AMPR, _______, KC_LPRN, KC_RPRN, KC_EQL,  KC_TILD, KC_PIPE, \
-  _______, _______, KC_NUHS,  KC_NUBS,   KC_CIRC, KC_ASTR, KC_PLUS, KC_LCBR, KC_RCBR, KC_MINS, KC_AT,   KC_GRV, \
-  _______, _______,S(KC_NUHS),S(KC_NUBS),_______, _______, _______, KC_LBRC, KC_RBRC, KC_INT3, KC_BSLS, KC_UNDS, \
-  _______, _______, _______,  _______,   _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
+  KC_EXLM,S(KC_QUOT),KC_HASH,  KC_DLR,    KC_PERC, KC_AMPR, KC_QUOT, KC_LPRN, KC_RPRN, KC_EQL,  KC_TILD, KC_PIPE, \
+  _______, _______,  KC_NUHS,  KC_NUBS,   KC_CIRC, KC_ASTR, KC_PLUS, KC_LCBR, KC_RCBR, KC_MINS, KC_AT,   KC_GRV, \
+  _______, _______, S(KC_NUHS),S(KC_NUBS),_______, _______, _______, KC_LBRC, KC_RBRC, KC_UNDS, KC_BSLS, _______, \
+  _______, _______,  _______,  _______,   _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
 ),
 
 /*[_LOWER] = KEYMAP( \
@@ -132,15 +136,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      |      |      |      |      |
+ * |      |      |      |      |             |      |      |      |      |      |UNDER |
+ * |      |      |      |      |             |      |      |      |      |      |GLOW  |
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] =  KEYMAP( \
   _______, RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL, \
+  _______, _______, _______, _______, _______, KC_1,    _______, _______, _______, _______, _______, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
-)
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, TG(_UNDERGLOW) \
+),
 
 /*
 RGB_TOG : RGBLED_TOGGLE: LED ON/OFF
@@ -152,14 +157,13 @@ RGB_SAD : RGBLED_DECREASE_SAT: 彩度の変更(-)
 RGB_VAI : RGBLED_INCREASE_VAL: 明るさの調整(+)
 RGB_VAD : RGBLED_DECREASE_VAL: 明るさの調整(-)
 */
-/*
-[UNDERGLOW] = KEYMAP( \
-  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______, \
+[_UNDERGLOW] = KEYMAP( \
   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______, \
   _______,  _______,  RGB_TOG,  RGB_MOD,  RGB_HUI,  RGB_HUD,  RGB_SAI,  RGB_SAD,  RGB_VAI,  RGB_VAD,  _______,  _______, \
+  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______, \
   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______ \
-),
-*/
+)
+
 };
 
 #ifdef AUDIO_ENABLE
